c76f0f8aa1631d6a916bae1550ee77dd035b9c29
migarjo <migarjo@github.com>
2018-09-05 15:09:18 -0600

    Update create-heroku-app.sh

--- Automation/create-heroku-app.sh
+++ Automation/create-heroku-app.sh
@@ -1,20 +1,17 @@
 app_name=$1
 
-# Check for existing pipelines on Heroku. If it already exists, delete it
-existing_pipelines=$(heroku pipelines)
-echo ${existing_pipelines}
+# Check for existing apps on Heroku. If it already exists, delete it
+existing_apps=$(heroku apps)
+echo ${existing_apps}
 while read -r line; do
   if [[ ${line} == "${app_name}" ]]; then
-    echo "Pipeline already exists. Destroying existing app..."
+    echo "App already exists. Destroying existing app..."
     heroku apps:destroy --app "${app_name}" -c "${app_name}"
   fi
-done <<< "${existing_pipelines}"
+done <<< "${existing_apps}"
 
 heroku apps:create migarjo-ruby-books --buildpack https://github.com/heroku/heroku-buildpack-ruby.git
 
-heroku pipelines:create ${app_name} \
-  --app=${app_name} \
-  --stage=production
 
 git push heroku master
 
